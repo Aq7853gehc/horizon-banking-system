52b8ddf27b3b86457a45e844804b6ca3e15b8d58
Aq7853gehc <aquib7583@gmail.com>
2024-09-21 19:29:59 +0530

    done the project

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import React from 'react'
 import BankCard from './BankCard'
 import { countTransactionCategories } from '@/lib/utils'
-import Category from './Category'
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   const categories: CategoryCount[] = countTransactionCategories(transactions);
@@ -31,7 +30,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
       <section className="banks">
         <div className="flex w-full justify-between">
           <h2 className="header-2">My Banks</h2>
-          <Link href="/" className="flex gap-2">
+          {/* <Link href="/" className="flex gap-2">
             <Image 
                src="/icons/plus.svg"
               width={20}
@@ -41,7 +40,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             <h2 className="text-14 font-semibold text-gray-600">
               Add Bank
             </h2>
-          </Link>
+          </Link> */}
         </div>
 
         {banks?.length > 0 && (
@@ -67,15 +66,6 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
           </div>
         )}
 
-        <div className="mt-10 flex flex-1 flex-col gap-6">
-          <h2 className="header-2">Top categories</h2>
-
-          <div className='space-y-5'>
-            {categories.map((category, index) => (
-              <Category key={category.name} category={category} />
-            ))}
-          </div>
-        </div>
       </section>
     </aside>
   )
